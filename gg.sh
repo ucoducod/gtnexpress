78df026921d3aef08aec88fe350199d56b9dae74
ucoducod <ucoducod@gmail.com>
2024-06-28 16:43:12 +0700

    Post-clone local fixed

--- gg.sh
+++ gg.sh
@@ -10,18 +10,19 @@ cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cl
 #git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
 #export PATH="$PATH:$HOME/flutter/bin"
 export PATH=/Users/hh/development/flutter/bin:$PATH
-
+flutter --version
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
-flutter precache --ios
+#flutter precache --ios
 
 # Install Flutter dependencies.
-flutter pub get
+#flutter pub get
 
 # Install CocoaPods using Homebrew.
-HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
-brew install cocoapods
+#HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
+#brew install cocoapods
 
 # Install CocoaPods dependencies.
-cd ios && pod install # run `pod install` in the `ios` directory.
+#cd ios && pod install # run `pod install` in the `ios` directory.
 
 exit 0
+
